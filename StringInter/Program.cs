19d87a7675dfcf95db3efb245179d295d99208f2
Opilane TTHK <opilane@tthk.ee>
2020-09-08 12:34:05 +0300

    first commit

---
+++ StringInter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            1. the app ask for the user´s first name and stores it in a seprate variable
+            2. the app ask for the user´s last name and stores it in a seprate variable
+            */
+
+            string FirstName;
+            string LastName;
+            Console.WriteLine("what is your name?");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("what is your last name");
+            LastName = Console.ReadLine();
+
+            //Console.WriteLine("Hello, " + FirstName + " " + LastName);
+
+            Console.WriteLine($"Hello, {FirstName} {LastName}");
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             1. Program asks for the user`s first and lastname
+            2. program asks for user`s year of birth
+            3. Program greets the user by their name and displays their age
+
+            */
+
+            string FirstName;
+            string LastName;
+            Console.WriteLine("what is your name?");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("what is your last name");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("What is your year of birth?");
+            string Userinput;
+            Userinput = Console.ReadLine();
+            int YearOfBirth;
+            YearOfBirth = Int32.Parse(Userinput);
+            int age = 2020 - YearOfBirth;
+
+
+            Console.WriteLine($"Hello, {FirstName} {LastName} {age} ");
+
+
+
+        }
+    }
+}
